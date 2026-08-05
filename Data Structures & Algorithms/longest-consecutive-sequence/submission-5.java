ccea8bbbea42c443f63c85aac6bc4ff9cf656f87
Ishaan Srivastava <134469620+Ishaan125@users.noreply.github.com>
2026-08-05 15:20:09 -0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int i : nums) {
+            heap.offer(i);
+        }
+
+        int max = 0;
+        int curr = 0;
+        int prev = Integer.MAX_VALUE-1;
+        while (!heap.isEmpty()) {
+            int val = heap.poll();
+            if (val == prev) {
+                continue;
+            }
+            if (val == prev+1) {
+                curr++;
+            } 
+            else {
+                curr = 1;
+            }
+            prev = val;
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+}
