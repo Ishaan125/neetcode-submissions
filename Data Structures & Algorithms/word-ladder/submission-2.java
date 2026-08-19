819f121b4c0a3ce789ce8140d7c33b0c8caa3bfa
Ishaan Srivastava <134469620+Ishaan125@users.noreply.github.com>
2026-08-19 15:09:18 -0700

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int res = 1;
+        LinkedList<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        boolean[] visited = new boolean[wordList.size()];
+
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                String word = q.poll();
+                if (word.equals(endWord)) {
+                    return res;
+                }
+
+                for (String nei : wordList) {
+                    if (!visited[wordList.indexOf(nei)] && check(word, nei)) {
+                        q.offer(nei);
+                        visited[wordList.indexOf(nei)] = true;
+                    }
+                }
+            }
+            res++;
+        }
+        return 0;
+    }
+
+    private boolean check(String word1, String word2) {
+        int dif = 0;
+        for (int i = 0; i < word1.length(); i++) {
+            if (word1.charAt(i) != word2.charAt(i)) {
+                dif++;
+            }
+            if (dif > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
