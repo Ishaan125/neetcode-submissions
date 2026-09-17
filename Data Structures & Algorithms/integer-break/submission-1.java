0cbf24a54baaebbfe9a2dc53355a856c8982f089
Ishaan Srivastava <134469620+Ishaan125@users.noreply.github.com>
2026-09-17 14:55:17 -0700

    Add: integer-break - submission-1

---
+++ Data Structures & Algorithms/integer-break/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int[] memo;
+    public int integerBreak(int n) {
+        memo = new int[n + 1];
+        return dfs(n);
+    }
+
+    private int dfs(int n) {
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        if (n == 1) {
+            return memo[n] = 1;
+        }
+        int max = 0;
+
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, i * Math.max(n - i, dfs(n - i)));
+        }
+
+        return memo[n] = max;
+    }
+}
